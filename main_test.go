8cf4a0bb995a71a2c913aa027c783b33f05023e3
Northes <45125250+Northes@users.noreply.github.com>
2022-01-11 15:18:39 +0800

    update

--- main_test.go
+++ main_test.go
@@ -5,13 +5,12 @@ import (
 )
 
 func TestGenerate(t *testing.T) {
-	//r := rand.New(rand.NewSource(time.Now().Unix()))
-	//i := r.Int()
-	//t.Log(i)
-	err := Generate(1)
-	if err != nil {
-		t.Error(err)
-		return
+	var err error
+	if err = Generate(1); err == nil {
+		t.Error("1 is ok,but return error")
 	}
-	t.Log("ok")
+
+	if err = Generate(0); err != nil {
+		t.Error("0 is not ok,but return nil")
+	}
 }
